dcfad5aac6880076fdc8c48793ee505b7556efc5
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-07 21:51:39 +0530

    Time: 1 ms (75.9%), Space: 53.8 MB (94.36%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.js
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.js
@@ -5,13 +5,16 @@
  */
 var countPrefixes = function(words, s) {
     // var str = words.join(" ");
-    var count=0;
-    for(item of words){
-        if(s.startsWith(item)){
-            count++;
-        }
-    }
-    return count;
+    // var count=0;
+    // for(item of words){
+    //     if(s.startsWith(item)){
+    //         count++;
+    //     }
+    // }
+    // return count;
+
+    var arr = words.filter((item)=>s.startsWith(item))
+    return arr.length
 };
 
 console.log(countPrefixes(["feh","w","w","lwd","c","s","vk","zwlv","n","w","sw","qrd","w","w","mqe","w","w","w","gb","w","qy","xs","br","w","rypg","wh","g","w","w","fh","w","w","sccy"],"w"))
