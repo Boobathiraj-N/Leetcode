e39934734a4e373c9ce60d2a14ab95f86d2af721
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-07 19:11:47 +0530

    Time: 6 ms (26.98%), Space: 60.1 MB (19.05%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.js
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.js
@@ -9,15 +9,15 @@ var areSimilar = function(mat, k) {
     for(let i =0;i<k;i++){  // loop for k times 
         for(let j=0;j<arr.length;j++){
             if(j%2 == 0){
-                arr[j].push(arr[j].shift())
+                // arr[j].push(arr[j].shift())
                 
-                // var num = arr[j].splice(arr[j][0-1],1)
-                // arr[j].push(...num)
+                var num = arr[j].splice(0,1)
+                arr[j].push(...num)
             }
             else{
-                arr[j].unshift(arr[j].pop())
-                // var num1 = arr[j].splice(arr[j][arr.length-2],1)
-                // arr[j].unshift(...num1)
+                // arr[j].unshift(arr[j].pop())
+                var num1 = arr[j].splice(-1,1)
+                arr[j].unshift(...num1)
             }
         }
     }
