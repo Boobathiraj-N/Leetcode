98aafb2a15b61af5bf10361323283e7a03ece4cf
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-08 10:42:08 +0530

    Time: 3 ms (16.98%), Space: 59 MB (7.55%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.js
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.js
@@ -8,7 +8,6 @@ var vowelStrings = function(arr, left, right) {
     var count=0;
     for(i=left;i<=right;i++){
         if(/^[aeiou].*[aeiou]$|^[aeiou]$/.test(arr[i])){
-                console.log(arr[i])
                 count++
         }
     }
