5bb2f1080fbe2506f54bdfc28069b19ad9b2da9c
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-08 10:46:36 +0530

    Time: 0 ms (100%), Space: 59 MB (7.55%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.js
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.js
@@ -4,11 +4,31 @@
  * @param {number} right
  * @return {number}
  */
-var vowelStrings = function(arr, left, right) {
+// var vowelStrings = function(arr, left, right) {
+//     var count=0;
+//     for(i=left;i<=right;i++){
+//         if(/^[aeiou].*[aeiou]$|^[aeiou]$/.test(arr[i])){
+//                 count++
+//         }
+//     }
+//     return count;
+// };
+
+// console.log(vowelStrings(["ou","e","e"],0,2))
+
+
+var vowelStrings = function(words, left, right) {
     var count=0;
+    var arr=['a','e','i','o','u']
     for(i=left;i<=right;i++){
-        if(/^[aeiou].*[aeiou]$|^[aeiou]$/.test(arr[i])){
+        // if(/^[aeiou].*[aeiou]$|^[aeiou]$/.test(arr[i])){
+        //         console.log(arr[i])
+        //         count++
+        // }
+        if(arr.includes(words[i][0])){
+            if(arr.includes(words[i][words[i].length-1])){
                 count++
+            }
         }
     }
     return count;
