dc0bc14e0d9f904dff35f3f682f99ff83ffd5df4
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-10 10:57:45 +0530

    Time: 5 ms (41.67%), Space: 63.6 MB (21.67%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.js
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.js
@@ -3,27 +3,42 @@
  * @return {number[]}
  */
 var rowAndMaximumOnes = function(mat) {
-  var res =[]
+  let index=0;
+  let count=0;
   for(i=0;i<mat.length;i++){
-      const obj = {}
       if(mat[i].includes(1)){
-          var arr = mat[i].filter((item)=> item == 1)
-          obj[i]=arr.length
-          obj.l=arr.length
-          res.push(obj)
-      }
-      else{
-        obj[i]=0;
-        obj.l=0
-        res.push(obj)
+          var arr = mat[i].filter((item)=>item ==1)
+          
+          if(arr.length > count){
+              index = i;
+              count=arr.length;
+          }
       }
   }
-res.sort((a,b)=>b.l-a.l)
-for(i in res[0])
-{
-    return [Number(i),res[0][i]]
-}
+  return [index,count]
 
 };
 
-console.log(rowAndMaximumOnes([[0,1],[1,0]]));
+console.log(rowAndMaximumOnes([[0,0],[1,1],[0,0]]));
+
+
+//   var res =[]
+//   for(i=0;i<mat.length;i++){
+//       const obj = {}
+//       if(mat[i].includes(1)){
+//           var arr = mat[i].filter((item)=> item == 1)
+//           obj[i]=arr.length
+//           obj.l=arr.length
+//           res.push(obj)
+//       }
+//       else{
+//         obj[i]=0;
+//         obj.l=0
+//         res.push(obj)
+//       }
+//   }
+// res.sort((a,b)=>b.l-a.l)
+// for(i in res[0])
+// {
+//     return [Number(i),res[0][i]]
+// }
