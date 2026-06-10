61160d14a53085dac9526216c5e1a1be4fa2f831
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-10 10:42:35 +0530

    Time: 15 ms (10%), Space: 63.8 MB (8.33%) - LeetHub

---
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+var rowAndMaximumOnes = function(mat) {
+  var res =[]
+  for(i=0;i<mat.length;i++){
+      const obj = {}
+      if(mat[i].includes(1)){
+          var arr = mat[i].filter((item)=> item == 1)
+          obj[i]=arr.length
+          obj.l=arr.length
+          res.push(obj)
+      }
+      else{
+        obj[i]=0;
+        obj.l=0
+        res.push(obj)
+      }
+  }
+res.sort((a,b)=>b.l-a.l)
+for(i in res[0])
+{
+    return [Number(i),res[0][i]]
+}
+
+};
+
+console.log(rowAndMaximumOnes([[0,1],[1,0]]));
