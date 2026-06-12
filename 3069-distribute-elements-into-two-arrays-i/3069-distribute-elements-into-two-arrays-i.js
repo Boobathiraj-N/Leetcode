131c8befbe84a64927cc91b8a0f296e36a11b2c8
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-12 10:52:29 +0530

    Time: 2 ms (58.97%), Space: 58.6 MB (7.69%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var resultArray = function(nums) {
+    let arr1=[]
+    let arr2=[]
+    for(let i=0;i<nums.length;i++){
+        if(i==0){
+            arr1.push(nums[i])
+        }
+        else if(i==1){
+            arr2.push(nums[i])
+        }
+        else{
+            if(arr1[arr1.length-1] > arr2[arr2.length-1]){
+                arr1.push(nums[i])
+            }
+            else{
+                arr2.push(nums[i])
+            }
+        }
+    }
+    return [...arr1,...arr2]
+};
