8d32f97155888e0717d6b8d019fb053085258c2c
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-14 07:42:28 +0530

    Time: 6 ms (16.83%), Space: 58 MB (5.94%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -5,13 +5,12 @@
 var minimumOperations = function(nums) {
     var count=0;
     for(j=0;j<nums.length;j++){
-        var minimum = Math.min(...nums.filter((item)=>{
-            return item !==0
-        }))
-        if (minimum == Infinity){
-            return count
-        }
-        else{
+        var str = nums.join("")
+        if(/[1-9]/.test(str)){
+            var minimum = Math.min(...nums.filter((item)=>{
+                return item !== 0
+            }))
+            
             for(i=0;i<nums.length;i++){
                 if(nums[i]-minimum<0){
                     nums[i]=0;
@@ -22,10 +21,43 @@ var minimumOperations = function(nums) {
             }
             count++
         }
-        
+        else{
+            break;
+        }
     }
     return count
     
 };
 
-console.log(minimumOperations([0]))
+console.log(minimumOperations([1,5,0,3,5]))
+
+
+
+
+// var minimumOperations = function(nums) {
+//     var count=0;
+//     for(j=0;j<nums.length;j++){
+//         var minimum = Math.min(...nums.filter((item)=>{
+//             return item !==0
+//         }))
+//         if (minimum == Infinity){
+//             return count
+//         }
+//         else{
+//             for(i=0;i<nums.length;i++){
+//                 if(nums[i]-minimum<0){
+//                     nums[i]=0;
+//                 }
+//                 else{
+//                     nums[i]=nums[i]-minimum
+//                 }
+//             }
+//             count++
+//         }
+        
+//     }
+//     return count
+    
+// };
+
+// console.log(minimumOperations([0]))
