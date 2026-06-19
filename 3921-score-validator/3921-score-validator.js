92be8aca1d1c0aa4c5679b739bde2dd0aab09cb2
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-19 08:40:27 +0530

    Time: 1 ms (83.73%), Space: 58.3 MB (18.93%) - LeetHub

--- 3921-score-validator/3921-score-validator.js
+++ 3921-score-validator/3921-score-validator.js
@@ -2,28 +2,28 @@
  * @param {string[]} events
  * @return {number[]}
  */
-var scoreValidator = function(events) {
-    var score=0;
-    var count=0;
-    for(item of events){
-        if(count<10){
-            if(!isNaN(item)){
-            score+=Number(item);
-            }
-            else{
-                if(item == "W"){
-                    if(count < 10){
-                        count++;
-                    }
-                    
-                }
-                else if(item == "WD" || item == "NB"){
-                    score++;
-                }
-            }
+function scoreValidator(events) {
+    let score = 0;
+    let counter = 0;
+
+    for (const event of events) {
+        // Condition 1: Counter 10 reach aana loop-ah niruthidanum
+        if (counter === 10) {
+            break;
+        }
+
+        // Condition 2: Event types-ah check panrom
+        if (event === "W") {
+            counter += 1;
+        } else if (event === "WD" || event === "NB") {
+            score += 1;
+        } else {
+            // String-ah number-ah mathi score kooda add panrom ("0", "1", "2", "3", "4", "6")
+            score += Number(event);
         }
     }
-    return [score,count]
-};
 
-console.log(scoreValidator(["W","W","W","W","W","W","W","W","W","W","W",6]));
+    return [score, counter];
+}
+
+console.log(scoreValidator(["1","4","W","6","WD"]));
