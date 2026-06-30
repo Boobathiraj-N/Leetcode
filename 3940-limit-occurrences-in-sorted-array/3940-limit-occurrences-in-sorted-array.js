7ec1b328e032e02c3742fe2380966c0e38a98c0f
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-06-30 10:14:34 +0530

    Time: 2 ms (51.72%), Space: 58 MB (60.69%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.js
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.js
@@ -4,21 +4,39 @@
  * @return {number[]}
  */
 
+// var limitOccurrences = function(nums, k) {
+//     var obj ={}
+//     var res=[]
+//     for(item of nums){
+//         if(obj[item]){
+//             if(obj[item]<k){
+//                 obj[item]++
+//                 res.push(item)
+//             }
+//         }
+//         else{
+//             obj[item]=1
+//             res.push(item)
+//         }
+//     }
+//     return res
+// };
+
 var limitOccurrences = function(nums, k) {
-    var obj ={}
-    var res=[]
-    for(item of nums){
-        if(obj[item]){
-            if(obj[item]<k){
-                obj[item]++
-                res.push(item)
-            }
-        }
-        else{
-            obj[item]=1
-            res.push(item)
-        }
-    }
+   var lastnum = nums[0]
+   var count =0
+   var res=[]
+   for(item of nums){
+       if(lastnum == item && count < k){
+           count++;
+           res.push(item)
+       }
+       else if(lastnum != item){
+           lastnum = item
+           count =1;
+           res.push(item)
+       }
+   }
     return res
 };
 console.log(limitOccurrences([1,2,3],1));
