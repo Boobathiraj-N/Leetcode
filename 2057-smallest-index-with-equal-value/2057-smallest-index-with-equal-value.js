f63d58f84deba12f7a5fc57f272295bdaa73a30d
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-07-01 10:03:15 +0530

    Time: 1 ms (21.62%), Space: 54.2 MB (94.59%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var smallestEqual = function(arr) {
+    var res=-1;
+    for(i=0;i<arr.length;i++){
+        if(i%10 == arr[i]){
+            if(res == -1){
+                res =i
+                break
+            }
+        }
+    }
+    return res
+};
+console.log(smallestEqual([6,5,4,4,9,1,5,0,8,8,5,8,0,9,8,3,6,5,2,7,7,6,6,8,9,6,5,6,5,6,8,6,9,5,1,0,5,5]));
