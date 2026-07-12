c4a6e9ccc6f9ee4301faf840389ff8ca82439866
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-07-12 14:02:32 +0530

    Time: 5 ms (2.72%), Space: 59.5 MB (39.54%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
@@ -2,12 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var concatWithReverse = function(nums) {
+//     var res =[...nums]
+//     for(i=nums.length-1;i>=0;i--){
+//         res.push(nums[i])
+//     }
+//     return res
+// };
+
+// console.log(concatWithReverse([1]));
+
 var concatWithReverse = function(nums) {
-    var res =[...nums]
-    for(i=nums.length-1;i>=0;i--){
-        res.push(nums[i])
-    }
-    return res
+    var arr =[...nums]
+    var reversed =nums.reverse()
+    return [...arr,...reversed]
 };
 
 console.log(concatWithReverse([1]));
