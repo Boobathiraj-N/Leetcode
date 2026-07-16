9ab7fbb3e7178c7e59676bcc951f09e70d25499a
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-07-16 10:11:57 +0530

    Time: 1 ms (40.43%), Space: 54.9 MB (93.62%) - LeetHub

--- 3861-minimum-capacity-box/3861-minimum-capacity-box.js
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.js
@@ -3,15 +3,41 @@
  * @param {number} itemSize
  * @return {number}
  */
+// var minimumIndex = function(capacity, itemSize) {
+//     var arr = [...capacity]
+//     capacity.sort((a,b)=>a-b)
+//     for(item of capacity){
+//         if(item >= itemSize){
+//             return arr.indexOf(item)
+//         }
+//     }
+//     return -1
+// };
+
+// console.log(minimumIndex([4],5))
+
+
 var minimumIndex = function(capacity, itemSize) {
-    var arr = [...capacity]
-    capacity.sort((a,b)=>a-b)
+    var res ;
     for(item of capacity){
-        if(item >= itemSize){
-            return arr.indexOf(item)
+        if(res){
+            if(item >= itemSize && item<res){
+                res =item
+            }
         }
+        else{
+            if(item >= itemSize){
+                res =item
+            }
+        }
+        
     }
-    return -1
+    if(res){
+        return capacity.indexOf(res)
+    }
+    else{
+        return -1
+    }
 };
 
-console.log(minimumIndex([4],5))
+console.log(minimumIndex([1,5,3,7],3))
