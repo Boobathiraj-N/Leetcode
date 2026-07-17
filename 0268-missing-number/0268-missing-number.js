da345e9f01d817550bde84e141426713df201750
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-07-17 20:05:21 +0530

    Time: 217 ms (5.36%), Space: 56 MB (69.71%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    for(i=0;i<=nums.length;i++){
+        if(!nums.includes(i)){
+            return i
+        }
+    }
+};
+
+console.log(missingNumber([3,0,1]));
