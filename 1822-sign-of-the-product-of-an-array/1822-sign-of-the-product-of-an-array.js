6362bae595d490174620afd5c02fc3a4b0b332c3
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-08-15 16:12:18 +0530

    Time: 1 ms (53.93%), Space: 56.6 MB (12.57%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    if(nums.includes(0)){
+        return 0
+    }
+    else{
+        var count=0
+        for(item of nums){
+            if(item < 0){
+                count++
+            }
+        }
+        if(count %2 == 0){
+            return 1
+        }
+        else{
+            return -1
+        }
+    }
+};
+
+console.log(arraySign([-1,1,-1,1,-1]))
