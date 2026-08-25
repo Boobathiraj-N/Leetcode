f2f838d4c4f7494ae3f43bc983e09a108ff522d5
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-08-25 08:25:40 +0530

    Time: 0 ms (100%), Space: 52.9 MB (89.02%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    var res = []
+    for(i=0;i<nums.length;i++){
+            res.splice(index[i],0,nums[i])
+        }
+    return res
+};
+
+console.log(createTargetArray([0,1,2,3,4],[0,1,2,2,1]))
