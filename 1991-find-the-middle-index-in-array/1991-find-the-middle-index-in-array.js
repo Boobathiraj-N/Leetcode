8f676e0c1b430ecfc437d9f80e513d17a0d5b2f4
Boobathiraj N <139227690+Boobathiraj-N@users.noreply.github.com>
2026-09-11 11:08:01 +0530

    Time: 0 ms (100%), Space: 55.8 MB (11.63%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function(nums) {
+    var totalsum =0
+    for(item of nums){
+      totalsum += item 
+    }
+    var sum =0;
+    var leftsum =0
+    var rightsum =0
+    for(i=0;i<nums.length;i++){
+      leftsum += nums[i-1] || 0
+      sum += nums[i]
+      rightsum = totalsum - sum
+      // console.log(`${i} leftsum = ${leftsum} sum = ${sum} rightsum = ${rightsum}`)
+      if(leftsum == rightsum){
+        return i
+      }
+    }
+    return -1
+};
+
+console.log(findMiddleIndex([2,3,-1,8,4]))
